e45d16e716dc7d9322a6dd46696007e50c23f090
admin <admin@axelor.com>
2018-03-15 10:44:41 +0100

    Fix archived records issues with relational fields
    - should show all archived records of o2m/m2m fields.
    - should not show archived records in m2o selection popups.
    
    Fixes RM-10986

--- axelor-web/src/main/webapp/js/form/form.relational.base.js
+++ axelor-web/src/main/webapp/js/form/form.relational.base.js
@@ -363,7 +363,6 @@ function RefFieldCtrl($scope, $element, DataSource, ViewService, initCallback) {
 		var params = {
 			filter: filter,
 			fields: fields,
-			archived: true,
 			sortBy: sortBy,
 			limit: limit
 		};
